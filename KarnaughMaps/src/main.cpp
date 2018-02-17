730bbefd172840d11d3bcf4cd22e370e60ec72d9
Gaurav Iyer <gaurav.iyer99@gmail.com>
2018-02-17 17:13:13 -0500

    Coded canCombine and next level

--- KarnaughMaps/src/main.cpp
+++ KarnaughMaps/src/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 class Implicant{
 	public:
 		bool isPrimeImplicant;
-
+		
 		string bitRep;
 		string key;
 
@@ -23,7 +23,7 @@ struct Node {
 
 
 //Consider finding the largest minterm and making a sufficient number of bits to represent this
-vector<Implicant> initializeMinterms(  vector<int> minterms  ) {
+vector<Implicant> initMinterms(  vector<int> minterms  ) {
 	vector<Implicant> firstLevel;
 
   	//Adding first level
@@ -45,13 +45,58 @@ vector<Implicant> initializeMinterms(  vector<int> minterms  ) {
 }
 
 
-Node* initializeQMc( vector<int> minterms ) {
-	Node *quineMc = new Node;
-	quineMc->level = initializeMinterms(  minterms  );
-	quineMc->next = nullptr;
-
+Node* createQMc( vector<int> minterms ) {
+	Node *quineMc = initNode();
+	quineMc->level = initMinterms(  minterms  );
+	
 	return quineMc;
 }
+
+
+Node* initNode() {
+	vector<Implicant> terms;
+	Node *nextNode = new Node;
+  nextNode->level = terms;
+  nextNode->next = nullptr;
+}
+
+
+bool canCombine( Implicant* num1, Implicant* num2 ) {
+		int numDifferences = 0;
+    
+		if( abs(num1->numOnes - num2->numOnes) != 1 ){
+    	return false;
+    } else {
+    	//Change 8 to be number bits dynamic in the future
+    	for(int i = 0; i < 8 ; i++){
+      	if( num1->bitRep.at(i) != num2->bitRep.at(i) )
+        	numDifferences++;
+      }
+    
+    	if( numDifferences == 1 )
+      	return true;
+        
+      else
+      	return false;
+    }
+}
+
+void createNextLevel( Node* prevNode ) {
+	int numTerms = prevNode->level.size();
+
+  Node* nextLevel = initNode();
+  prevNode->next = nextLevel;
+  
+	for ( int i = 0; i < numTerms-1; i++ ) {
+  	for ( int j = i+1; j < numTerms; j++ ) {
+    	if( canCombine( quineMc->level.at(i) , quineMc->level.at(j) ){
+      	
+      }
+    }
+  }
+	
+}
+	
 
 
 int main( void ) {
@@ -59,9 +104,10 @@ int main( void ) {
 	minterms.push_back(2);
 	minterms.push_back(4);
 	minterms.push_back(9);
-	Node *a = initializeQMc( minterms );
+	Node *a = createQMc( minterms );
 
 	cout << a->level.at(0).bitRep << endl;
+	cout << a->level.at(2).bitRep << endl;
 }
 
 
