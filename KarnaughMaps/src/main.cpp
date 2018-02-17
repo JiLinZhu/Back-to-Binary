fd5d06ac55be9b01f91b66c2c9b6c132f0cccaed
Ji Lin Zhu <jilinzhu.canada@gmail.com>
2018-02-17 16:18:35 -0500

    Minterm Level of QMc complete

--- KarnaughMaps/src/main.cpp
+++ KarnaughMaps/src/main.cpp
@@ -11,9 +11,14 @@ using namespace std;
 #include <vector>
 
 
+<<<<<<< HEAD
+class Implicant{
+	public:
+=======
  class Implicant{
 	public:
 	
+>>>>>>> 0a88676bb82d56dcc2d4bf27e142614c7299fc94
 		bool isPrimeImplicant;
 
 		string bitRep;
@@ -24,7 +29,11 @@ using namespace std;
 };
 
 struct Node {
+<<<<<<< HEAD
+	vector<Implicant> level;
+=======
 	vector<Implicant*> level;
+>>>>>>> 0a88676bb82d56dcc2d4bf27e142614c7299fc94
 	Node* next;
 };
 
@@ -35,6 +44,18 @@ vector <Implicant*> initializeMinterms(  vector<int> minterms  ) {
 
   	//Adding first level
 	for ( int i = 0; i < minterms.size(); i++ ) {
+<<<<<<< HEAD
+		Implicant minterm;
+		int numOnes = 0;
+
+		minterm.isPrimeImplicant = true;
+		minterm.bitRep = bitset<8>( minterms.at(i) ).to_string();
+
+		//Counting how many ones are in the bit representation
+		for(int k = 0; k < 8; k++) if( minterm.bitRep.at(k) == 1 ) numOnes++;
+
+		minterm.numOnes = numOnes;
+=======
 		Implicant *minterm = new Implicant();
 		int numOnes = 0;
 
@@ -45,6 +66,7 @@ vector <Implicant*> initializeMinterms(  vector<int> minterms  ) {
 		for(int k = 0; k < 8; k++) if(minterm->bitRep.at(k) == 1) numOnes++;
 
 		minterm->numOnes = numOnes;
+>>>>>>> 0a88676bb82d56dcc2d4bf27e142614c7299fc94
 
 		firstLevel.push_back(minterm);
 	}
@@ -52,15 +74,24 @@ vector <Implicant*> initializeMinterms(  vector<int> minterms  ) {
 }
 
 
-Node* initializeQuineMccluskey( vector<int> minterms ) {
+Node* initializeQMc( vector<int> minterms ) {
 	Node *quineMc = new Node;
 	quineMc->level = initializeMinterms(  minterms  );
 	quineMc->next = nullptr;
+
+	return quineMc;
 }
 
 
+int main( void ) {
+	vector<int> minterms;
+	minterms.push_back(2);
+	minterms.push_back(4);
+	minterms.push_back(9);
+	Node *a = initializeQMc( minterms );
 
-
+	cout << a->level.at(0).bitRep << endl;
+}
 
 
 
